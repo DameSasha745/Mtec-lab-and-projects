a865223c32503d118e537d42460e3a8e15e25904
Sasha Cummings <pheobistorm@gmail.com>
2025-02-13 07:27:00 -0700

    new update

--- mTec/lab Calculater/lab Calculater/ViewController.swift
+++ mTec/lab Calculater/lab Calculater/ViewController.swift
@@ -8,12 +8,83 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var textLabel: UILabel!
+    
+    var currentText: String = ""
+    var previousText: String = ""
+    var currentOP: String?
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textLabel.text = "0"
     }
+    
+    @IBAction func numberTyped(_ sender: UIButton) {
+        guard let senderButton = sender.titleLabel?.text else { return }
+    
+    
+            switch senderButton {
 
+                
+            case "0", "1", "2", "3", "4", "5", "6", "7", "8", "9":
+                currentText += senderButton
+                textLabel.text = currentText
+                
 
+            case "÷", "x", "+", "-":
+                if !currentText.isEmpty {
+                    previousText = currentText
+                    currentText = ""
+                }
+                currentOP = senderButton
+                textLabel.text = currentText
+                
+            case ".":
+                if !currentText.contains(".") {
+                    currentText += "."
+                    textLabel.text = currentText
+                }
+                
+            case "=":
+                if let Operator = currentOP, !previousText.isEmpty, !currentText.isEmpty {
+                    let total = caluculateResult()
+                    textLabel.text = "\(total)"
+                    previousText = "\(total)"
+                    currentText = ""
+                    currentOP = nil
+                }
+            case "AC":
+                currentText = ""
+                previousText = ""
+                currentOP = nil
+                textLabel.text = "0"
+                
+                
+            default:
+                break
+                
+            }
+        }
+        func caluculateResult() -> Double {
+            guard let operatorSymbol = currentOP, let previous = Double(previousText), let current = Double(currentText) else {
+                return 0
+            }
+            
+            switch operatorSymbol {
+            case "+":
+                return previous + current
+            case "-":
+                return previous - current
+            case "x":
+                return previous * current
+            case "÷":
+                return previous / current
+            default:
+                return 0
+        }
+            
+            
+    }
 }
-
--- mTec/lab project Apple pie/lab project Apple pie/ViewController.swift
+++ mTec/lab project Apple pie/lab project Apple pie/ViewController.swift
@@ -13,10 +13,16 @@ class ViewController: UIViewController {
     
     let incorrectMovesAllowed = 7
     
-    var totalWins = 0
-    
-    var totalLosses = 0
-
+    var totalWins = 0 {
+        didSet {
+            newRound()
+        }
+    }
+    var totalLosses = 0 {
+        didSet {
+            newRound()
+        }
+    }
     @IBOutlet weak var treeImageView: UIImageView!
     
     @IBOutlet weak var correctWordLabel: UILabel!
@@ -33,15 +39,31 @@ class ViewController: UIViewController {
 }
     var currentGame: Game!
     
+    func enableLetterButton(_ enable: Bool) {
+        for button in letterButtons {
+            button.isEnabled = enable
+        }
+    }
+    
     
     func newRound() {
-        let newWord = listOfWords.removeFirst()
-        currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
-        updateUI()
+        if !listOfWords.isEmpty {
+            let newWord = listOfWords.removeFirst()
+            currentGame = Game(word: newWord, incorrectMovesRemaining: incorrectMovesAllowed, guessedLetters: [])
+            enableLetterButton(true)
+            updateUI()
+        } else {
+            enableLetterButton(false)
+        }
     }
     
     func updateUI() {
-        correctWordLabel.text = currentGame.formattedWord
+        var letters = [String]()
+        for letter in currentGame.formattedWord {
+            letters.append(String(letter))
+        }
+        let wordWithSpacing = letters.joined(separator: " ")
+        correctWordLabel.text = wordWithSpacing
         scoreLabel.text = "Wins: \(totalWins), Losses: \(totalLosses)"
         treeImageView.image = UIImage(named: "Tree \(currentGame.incorrectMovesRemaining)")
     }
@@ -56,6 +78,18 @@ class ViewController: UIViewController {
         let letter = Character(letterString.lowercased())
         currentGame.playerGuessed(letter: letter)
         updateUI()
+    }
+    
+    
+    
+    func updadteGameStatus() {
+        if currentGame.incorrectMovesRemaining == 0 {
+            totalLosses += 1
+        } else if currentGame.word == currentGame.formattedWord {
+            totalWins += 1
+        } else {
+            updateUI()
+        }
     }
 }
 
